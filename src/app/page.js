e6ef5a6211c178fd37418c47269537c4c20d9f5f
Heestolee <dlckdgml25@gmail.com>
2024-09-20 06:35:32 +0900

    Feat: Add static intro UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,47 @@
 "use client";
 
+import Image from "next/image";
+import React, { useState } from "react";
+import DeployModeSelector from "@/components/DeployModeSelector";
+import InputArea from "@/components/InputArea";
+
 export default function Home() {
+  const [deployMode, setDeployMode] = useState("url");
+  const [url, setUrl] = useState("");
+  const [customContent, setCustomContent] = useState("");
+  const [previewMode, setPreviewMode] = useState(false);
+  const [pageId, setPageId] = useState(null);
+
+  const handleFetch = () => {
+    if (deployMode === "url") {
+      const id = url.split("/").pop();
+      setPageId(id);
+    }
+    setPreviewMode(true);
+  };
+
   return (
-    <div> Main page </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="mb-6">
+        <Image
+          src="/notiondrop.png"
+          alt="notiondrop logo"
+          width={800}
+          height={400}
+        />
+      </div>
+      <DeployModeSelector
+        deployMode={deployMode}
+        setDeployMode={setDeployMode}
+      />
+      <InputArea
+        deployMode={deployMode}
+        url={url}
+        setUrl={setUrl}
+        customContent={customContent}
+        setCustomContent={setCustomContent}
+        handleFetch={handleFetch}
+      />
+    </div>
   );
 }
