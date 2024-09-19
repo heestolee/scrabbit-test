72e6baa7500553c63e92ca53af3ada81b7acdc9f
Heestolee <dlckdgml25@gmail.com>
2024-09-20 05:55:01 +0900

    Chore: Configure nextConfig, vercel for SSR

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  experimental: {
+    esmExternals: false,
+    appDir: true,
+  },
+};
 
 export default nextConfig;
