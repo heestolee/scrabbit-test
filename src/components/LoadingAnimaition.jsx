958dba16a3e4d2e619c3767b33110e7c865210d6
Heestolee <dlckdgml25@gmail.com>
2024-09-29 05:29:02 +0900

    Feat: Implement LoadingAnimation

---
+++ src/components/LoadingAnimaition.jsx
@@ -0,0 +1,20 @@
+import { motion } from "framer-motion";
+
+export default function LoadingAnimation() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "45rem", width: "100%"}}>
+      <motion.img
+        src="/notiondrop.svg"
+        alt="Loading"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: [0, 1, 0] }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "loop",
+        }}
+      />
+    </div>
+  );
+};
